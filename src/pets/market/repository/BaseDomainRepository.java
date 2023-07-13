3eb0ebff4c5e16259dbe397f04ce0ed919616f21
Araya2001 <araya2001@outlook.es>
2023-07-12 18:22:10 -0600

    Persist impl

---
+++ src/pets/market/repository/BaseDomainRepository.java
@@ -0,0 +1,26 @@
+package pets.market.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public interface BaseDomainRepository<T, S> {
+    Optional<T> findById(S id);
+
+    List<T> findAll();
+
+    List<T> findByPredicate(Predicate<T> tPredicate);
+
+    List<T> saveAll(List<T> list);
+
+    List<T> updateAll(List<T> list);
+
+    T save(T t);
+
+    T update(T t);
+
+    Boolean delete(T t);
+
+    Boolean deleteAll(List<T> list);
+
+}
