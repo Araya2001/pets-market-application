40fafffdffc8f4899bb5a72c18455928d9148541
morabdiana <diana_mora_b@hotmail.com>
2023-08-13 16:51:59 -0600

    JOptionPane ventanas emergentes de bienvenida y escogencia de opciones dependiendo de Rol
    agregué mensajes de bienvenida y de escogencia de opciones dependiendo del login que se haga ya sea como vendedor o gerente

--- src/pets/market/repository/InventoryItemRepository.java
+++ src/pets/market/repository/InventoryItemRepository.java
@@ -3,4 +3,4 @@ package pets.market.repository;
 import pets.market.domain.Sale;
 
 public interface InventoryItemRepository extends BaseDomainRepository<Sale, Long> {
-}
+} // acá en lugar de "SALE" no deberia de ir "InventoryItem"?
