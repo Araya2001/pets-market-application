a9ba0311a68a42a747e69175c666f82ebe154e9e
Vale <valeriavega270@gmail.com>
2023-08-23 00:14:34 -0600

    vale
    alimentos + articulos

--- src/pets/market/PetsMarketApplication.java
+++ src/pets/market/PetsMarketApplication.java
@@ -37,7 +37,53 @@ public class PetsMarketApplication {
     // BLOQUE PRODUCTOS
     InventoryItem inventoryItem1 = new InventoryItem();
     inventoryItem1.setPetType("PERRO").setItemType("MEDICINA");
+    
+    
+    
+    
+    
+    
+    
+    InventoryItem inventoryItem11 = new InventoryItem();
+    inventoryItem11.setPetType("GATO").setItemType("ALIMENTOS").setQuantity(10).setPriceValue(3000.00).setName("ATUN").setId("001-ALI-ATUN");
+    
+    InventoryItem inventoryItem12 = new InventoryItem();
+    inventoryItem12.setPetType("PERRO").setItemType("ALIMENTOS").setQuantity(10).setPriceValue(5000.00).setName("CROQUETAS").setId("002-ALI-CROQUETAS");
+ 
+    InventoryItem inventoryItem13 = new InventoryItem();
+    inventoryItem13.setPetType("GATO").setItemType("ALIMENTOS").setQuantity(10).setPriceValue(1000.00).setName("HELADO").setId("003-ALI-HELADO");
+    
+    InventoryItem inventoryItem14 = new InventoryItem();
+    inventoryItem14.setPetType("PERRO").setItemType("ALIMENTOS").setQuantity(10).setPriceValue(10000.00).setName("ALIMENTO CACHORRO").setId("004-ALI-CACHORRO");
+    
+    InventoryItem inventoryItem15 = new InventoryItem();
+    inventoryItem15.setPetType("PERRO").setItemType("ALIMENTOS").setQuantity(10).setPriceValue(4000.00).setName("SNACK").setId("005-ALI-SNACK");
+    
+    InventoryItem inventoryItem16 = new InventoryItem();
+    inventoryItem11.setPetType("GATO").setItemType("ARTICULOS").setQuantity(10).setPriceValue(3000.00).setName("COLLAR").setId("001-ART-COLLAR");
+    
+    InventoryItem inventoryItem17 = new InventoryItem();
+    inventoryItem12.setPetType("PERRO").setItemType("ARTICULOS").setQuantity(10).setPriceValue(5000.00).setName("RATON").setId("002-ART-RATON");
+ 
+    InventoryItem inventoryItem18 = new InventoryItem();
+    inventoryItem13.setPetType("GATO").setItemType("ARTICULOS").setQuantity(10).setPriceValue(1000.00).setName("HUESO PERRO").setId("003-ART-HUESO");
+    
+    InventoryItem inventoryItem19 = new InventoryItem();
+    inventoryItem14.setPetType("PERRO").setItemType("ARTICULOS").setQuantity(10).setPriceValue(10000.00).setName("HARNES").setId("004-ART-HARNES");
+    
+    InventoryItem inventoryItem20 = new InventoryItem();
+    inventoryItem15.setPetType("PERRO").setItemType("ARTICULOS").setQuantity(10).setPriceValue(4000.00).setName("PELOTA").setId("005-ART-PELOTA");
+    
     inventoryRepository.save(inventoryItem1);
+    inventoryRepository.save(inventoryItem12);
+    inventoryRepository.save(inventoryItem13);
+    inventoryRepository.save(inventoryItem14);
+    inventoryRepository.save(inventoryItem15);
+    inventoryRepository.save(inventoryItem16);
+    inventoryRepository.save(inventoryItem17);
+    inventoryRepository.save(inventoryItem18);
+    inventoryRepository.save(inventoryItem19);
+    inventoryRepository.save(inventoryItem20);
 
     while (gui.doRequestYesNoMenu("Desea ingresar a la aplicación?")) {
       User user = loginMenu.doLogin();
